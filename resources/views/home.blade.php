de008583183b897f03271cf7135e70fa093d4043
Henrique Costa <henriquecostapsilva@gmail.com>
2025-12-03 18:07:25 -0300

    feat: adding first route

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,5 +1,6 @@
-<x-layout>
+<x-layout.app>
     <div>
+        teste
         <!-- I have not failed. I've just found 10,000 ways that won't work. - Thomas Edison -->
     </div>
-</x-layout>
+</x-layout.app>
