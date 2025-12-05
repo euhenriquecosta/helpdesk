901fd892e6a04bbc37d4e8020ea426aa6e81c586
Henrique Costa <henriquecostapsilva@gmail.com>
2025-12-05 13:37:38 -0300

    feat: styling the register page to make similar to login

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -61,30 +61,4 @@
             </div>
         </div>
     </main>
-</div>
-
-
-    <!--<h1>Login</h1>
-
-    @if (session('message'))
-        <span>{{ session('message') }}</span>
-    @endif
-
-    <form action="{{ route('login') }}" method="POST">
-        @csrf
-
-        <label for="email">Email</label>
-        <input type="email" name="email" id="email" value="{{ old('email') }}" required>
-        @error('email')
-            <span>{{ $message }}</span>
-        @enderror
-
-        <label for="password">Password</label>
-        <input type="password" name="password" id="password" required>
-        @error('password')
-            <span>{{ $message }}</span>
-        @enderror
-
-        <button type="submit">Login</button>
-    </form>-->
 </x-layout.app>
